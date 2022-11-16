4beba5a96557d4898a37c3d0ebd7ce4d92ffd045
Elias de Souza <elias99-souza@hotmail.com>
2022-11-16 09:49:03 -0300

    att

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import PaginaInicial from './pages';
 import RotasWeb from './routs';
-
+import style from './index.module.scss'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <React.StrictMode >
 
-    <RotasWeb />
+    <div className={style.module}>
+      <RotasWeb />
+    </div>
 
 
   </React.StrictMode>
