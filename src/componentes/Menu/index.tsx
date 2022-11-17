aa204afa0bfdc82ff135eff172db55cbb79e81cb
Elias de Souza <elias99-souza@hotmail.com>
2022-11-17 12:10:45 -0300

    atualizacaoProjeto

--- src/componentes/Menu/index.tsx
+++ src/componentes/Menu/index.tsx
@@ -11,16 +11,18 @@ export default function Menu() {
     }, {
         label: 'Restaurante',
         to: '/restaurante'
-    },{
+    }, {
         label: 'Administração',
-        to:'/administracao'
+        to: '/administracao'
     }
 
     ]
 
+
+
     return (
         <div className={style.module}>
-            <ul >
+            <ul    >
                 {rotas.map((rota, index) => (
                     <li key={index}>
                         <Link to={rota.to}>
@@ -29,6 +31,21 @@ export default function Menu() {
                     </li>
                 ))}
             </ul>
+
+            <ol>
+                <li><a href="#p1">Ir para o primeiro parágrafo!</a></li>
+                <li><a href="#p2">Ir para o segundo parágrafo!</a></li>
+            </ol>
+
+            <h3>Meu artigo divertido</h3>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident aspernatur quae facere saepe ea eaque tempora quo? Ratione cupiditate excepturi similique animi praesentium possimus magni laboriosam obcaecati maiores enim!
+
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident aspernatur quae facere saepe ea eaque tempora quo? Ratione cupiditate excepturi similique animi praesentium possimus magni laboriosam obcaecati maiores enim!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident aspernatur quae facere saepe ea eaque tempora quo? Ratione cupiditate excepturi similique animi praesentium possimus magni laboriosam obcaecati maiores enim!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident aspernatur quae facere saepe ea eaque tempora quo? Ratione cupiditate excepturi similique animi praesentium possimus magni laboriosam obcaecati maiores enim!Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident aspernatur quae facere saepe ea eaque tempora quo? Ratione cupiditate excepturi similique animi praesentium possimus magni laboriosam obcaecati maiores enim!Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident aspernatur quae facere saepe ea eaque tempora quo? Ratione cupiditate excepturi similique animi praesentium possimus magni laboriosam obcaecati maiores enim!Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident aspernatur quae facere saepe ea eaque tempora quo? Ratione cupiditate excepturi similique animi praesentium possimus magni laboriosam obcaecati maiores enim!Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident aspernatur quae facere saepe ea eaque tempora quo? Ratione cupiditate excepturi similique animi praesentium possimus magni laboriosam obcaecati maiores enim!vLorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident aspernatur quae facere saepe ea eaque tempora quo? Ratione cupiditate excepturi similique animi praesentium possimus magni laboriosam obcaecati maiores enim!Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident aspernatur quae facere saepe ea eaque tempora quo? Ratione cupiditate excepturi similique animi praesentium possimus magni laboriosam obcaecati maiores enim!Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident aspernatur quae facere saepe ea eaque tempora quo? Ratione cupiditate excepturi similique animi praesentium possimus magni laboriosam obcaecati maiores enim!Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident aspernatur quae facere saepe ea eaque tempora quo? Ratione cupiditate excepturi similique animi praesentium possimus magni laboriosam obcaecati maiores enim!Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident aspernatur quae facere saepe ea eaque tempora quo? Ratione cupiditate excepturi similique animi praesentium possimus magni laboriosam obcaecati maiores enim!Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident aspernatur quae facere saepe ea eaque tempora quo? Ratione cupiditate excepturi similique animi praesentium possimus magni laboriosam obcaecati maiores enim!Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident aspernatur quae facere saepe ea eaque tempora quo? Ratione cupiditate excepturi similique animi praesentium possimus magni laboriosam obcaecati maiores enim!Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident aspernatur quae facere saepe ea eaque tempora quo? Ratione cupiditate excepturi similique animi praesentium possimus magni laboriosam obcaecati maiores enim!Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident aspernatur quae facere saepe ea eaque tempora quo? Ratione cupiditate excepturi similique animi praesentium possimus magni laboriosam obcaecati maiores enim!Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident aspernatur quae facere saepe ea eaque tempora quo? Ratione cupiditate excepturi similique animi praesentium possimus magni laboriosam obcaecati maiores enim!Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident aspernatur quae facere saepe ea eaque tempora quo? Ratione cupiditate excepturi similique animi praesentium possimus magni laboriosam obcaecati maiores enim!Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident aspernatur quae facere saepe ea eaque tempora quo? Ratione cupiditate excepturi similique animi praesentium possimus magni laboriosam obcaecati maiores enim!Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident aspernatur quae facere saepe ea eaque tempora quo? Ratione cupiditate excepturi similique animi praesentium possimus magni laboriosam obcaecati maiores enim!Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident aspernatur quae facere saepe ea eaque tempora quo? Ratione cupiditate excepturi similique animi praesentium possimus magni laboriosam obcaecati maiores enim!Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident aspernatur quae facere saepe ea eaque tempora quo? Ratione cupiditate excepturi similique animi praesentium possimus magni laboriosam obcaecati maiores enim!Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident aspernatur quae facere saepe ea eaque tempora quo? Ratione cupiditate excepturi similique animi praesentium possimus magni laboriosam obcaecati maiores enim! Distinctio provident aspernatur quae facere saepe ea eaque tempora quo? Ratione cupiditate excepturi similique animi praesentium possimus magni laboriosam obcaecati maiores enim!Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident aspernatur quae facere saepe ea eaque tempora quo? Ratione cupiditate excepturi similique animi praesentium possimus magni laboriosam obcaecati maiores enim!Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident aspernatur quae facere saepe ea eaque tempora quo? Ratione cupiditate excepturi similique animi praesentium possimus magni laboriosam obcaecati maiores enim!Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident aspernatur quae facere saepe ea eaque tempora quo? Ratione cupiditate excepturi similique animi praesentium possimus magni laboriosam obcaecati maiores enim!Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident aspernatur quae facere saepe ea eaque tempora quo? Ratione cupiditate excepturi similique animi praesentium possimus magni laboriosam obcaecati maiores enim!
+            <p id="p1">Você pode definir este parágrafo como alvo usando um fragmento de URL.</p>
+            <p id="p2">Esse é <i>outro parágrafo</i>, também acessível
+                pelos links acima.</p>
 
         </div>
     )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,19 @@
 import Lista from "./Lista"
 
- 
- 
+
+
 export default function PaginaInicial() {
- 
+    const Pessoas = [{
+        nome: 'Cliente1',
+        idade: 39
+    }]
+
     return (
         <div>
 
             <h1>Restaurante Global</h1>
-         
+            <Lista pessoas={Pessoas} />
+
 
 
 
