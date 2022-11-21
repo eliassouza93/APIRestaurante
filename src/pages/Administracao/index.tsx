76e6e837a4beacc2ed588fa191f75dc96b22c8d5
Elias de Souza <elias99-souza@hotmail.com>
2022-11-21 08:58:11 -0300

    atProjeto

--- src/pages/Administracao/index.tsx
+++ src/pages/Administracao/index.tsx
@@ -3,20 +3,25 @@ import { useState } from "react"
 
 export default function Administracao() {
     const [estado, setEstado] = useState('')
+    
+    const [pegar, setPegar] = useState('')
 
-    function 
-
+    function getPegar() {
+        setPegar(estado)
+    }
+    function mostrar(event: any) {
+        event?.preventDefault()
+        getPegar()
+    }
     return (
         <div>
             <h1>Seção Administração</h1>
-
-            <form>
-                <input onChange={(e) => setEstado (e.target.value)} type="text" />
-              
-                <button>cadastrar</button>
+            <form onSubmit={mostrar}>
+                <input onChange={(e) => setEstado(e.target.value)} type="text" placeholder="digíte seu nome" required />
+                <input type="text" placeholder="data de nascimento" />
+                <button >cadastrar</button>
             </form>
-
-
+            <h2>Nome:{pegar}</h2>
         </div>
     )
 }
